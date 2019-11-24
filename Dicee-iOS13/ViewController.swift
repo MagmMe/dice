4e3d6d570eb8d9f2d11903ad12aa5c03fc38e71d
MagmMe <marcin@magm.me>
2019-11-24 07:05:58 +0100

    working with properties

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -16,8 +16,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
-        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
+        diceImageView1.image = #imageLiteral(resourceName: "DiceFour")
+        diceImageView1.alpha = 0.5
+        diceImageView2.image = #imageLiteral(resourceName: "DiceSix")
     }
 
 
